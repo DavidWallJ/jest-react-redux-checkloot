290c5f8c6f56917b0fb3960d163fd7dc3337453f
DavidWallJ <djkindread@gmail.com>
2017-12-10 17:53:45 +0800

    setup balance action and test file

---
+++ src/actions/balance.test.js
@@ -0,0 +1,9 @@
+import * as actions from './balance';
+
+it('creates an action to set the balance', () => {
+	const balance = 0;
+
+	const expectedAction = { type: 'SET_BALANCE', balance };
+
+	expect(actions.setBalance(balance)).toEqual(expectedAction);
+});
