f7d268fa8b8c7dd0b88446fb705c30b3cd47687a
DavidWallJ <djkindread@gmail.com>
2017-12-16 17:02:49 +0800

    added some styling

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,13 @@ const App = () => {
 			<p className="jestCoverage">
 				* Fully Jest tested to a coverage of %95.12
 			</p>
+			<a
+				className="githubLink"
+				href="https://github.com/DavidWallJ/jest-react-redux-checkloot"
+				target="_blank"
+			>
+				View the code on Github
+			</a>
 		</div>
 	);
 };
